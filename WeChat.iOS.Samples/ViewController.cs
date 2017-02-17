a485f6dd8813a99f3eaa947765ba8420cb201042
Kinfey <lokinfey@outlook.com>
2017-02-17 23:07:23 +0800

    WeChatBinding
    微信sdk for iOS 绑定例子
    This is Sample for WeChat SDK iOS Binding

---
+++ WeChat.iOS.Samples/ViewController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using UIKit;
+
+namespace WeChat.iOS.Samples
+{
+	public partial class ViewController : UIViewController
+	{
+
+		WeChatAPI we;
+
+		protected ViewController(IntPtr handle) : base(handle)
+		{
+			// Note: this .ctor should not contain any initialization logic.
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+			we = new WeChatAPI();
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+
+		partial void UIButton3_TouchUpInside(UIButton sender)
+		{
+			we.SendText("Hello Xamarin@WeChat");
+		}
+	}
+}
---
+++ WeChat.iOS.Samples/WeChatAPI.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Foundation;
+using WeChat.iOS;
+
+namespace WeChat.iOS.Samples
+{
+	public class WeChatAPI: WXApiDelegate
+	{
+		//微信登录
+		public bool Log(string appID)
+		{
+			var result = WXApi.RegisterApp(appID);
+			return result;
+		}
+		//微信链接打开
+		public bool Open(NSUrl url)
+		{
+			var result = WXApi.HandleOpenURL(url, this);
+			return result;
+		}
+		//请求打开微信
+		public override void OnReq(BaseReq req)
+		{
+
+		}
+		//响应微信
+		public override void OnResp(BaseResp resp)
+		{
+
+		}
+
+		//发送信息到朋友圈
+		public bool SendText(string text)
+		{
+			SendMessageToWXReq req = new SendMessageToWXReq();
+			req.Text = text;
+			req.BText = true;
+			req.Scene = 1;
+			WXApi.SendReq(req);
+
+			return true;
+		}
+
+	}
+}
